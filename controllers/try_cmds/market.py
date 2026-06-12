95666e7b5b1ea4e6c1bf2e2c46ca60432f9b1bb3
Bentoza <paninkirill219@gmail.com>
2026-06-12 15:43:32 +0300

    deleted test loggers

--- controllers/try_cmds/market.py
+++ controllers/try_cmds/market.py
@@ -47,9 +47,7 @@ class MarketResult:
 def checking_for_prepared_options(amount, balance):
     """Checking if user chose on of the predicted answers"""
     if amount in MARKET_MULTIPLIERS.keys():
-        logger.info("True")
         amount = MARKET_MULTIPLIERS[amount]*balance
-    logger.info(amount)
     return amount
 
 def _user_checks(amount, balance, status, time_since_last):
@@ -132,9 +130,7 @@ async def logic(interaction_user_id, interaction_guild_id, amount: int = None):
             time_since_last = get_timestamp() - market_timestamp
 
 
-        logger.info(amount)
         amount = checking_for_prepared_options(amount, balance) # if player chose 75%, 50%, all-in and e.t.c
-        logger.info(amount)
         check = _user_checks(amount, balance, status, time_since_last)
         if check:
             return check
