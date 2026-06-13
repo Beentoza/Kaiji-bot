4af90ebb554a0d2cafe48695ede5d4aaec5618cd
Bentoza <paninkirill219@gmail.com>
2026-06-13 16:57:43 +0300

    Moved double probs to constants. And added dynamic chance to win for double_or_nothing. And changed it from 50% to 48%

--- controllers/try_cmds/double.py
+++ controllers/try_cmds/double.py
@@ -10,8 +10,6 @@ from helpers.user_functions.check_ban import is_banned
 import constants
 
 
-LOWEST_DOUBLE_PROB = 1000
-HIGHEST_DOUBLE_PROB = 2000
 
 
 class DoubleOutcome(enum.Enum):
@@ -37,11 +35,13 @@ class DoubleResult:
 
 def _resolve_double_outcome(double_prob: int, amount: int) -> tuple[None, None] | tuple[int, bool]:
     """Deciding win or lose from probability, returns delta and won flag"""
-    prob_to_win = (HIGHEST_DOUBLE_PROB + LOWEST_DOUBLE_PROB)*0.5
-    if double_prob < prob_to_win:
+    # example: min: 1, high: 10, 40%
+    # 1 + (10-1)*0.4 = 4.6 - only 4 numbers will pass
+    prob_to_win = constants.LOWEST_DOUBLE_PROB + (constants.HIGHEST_DOUBLE_PROB - constants.LOWEST_DOUBLE_PROB) * constants.DOUBLE_CHANCE_TO_WIN * 0.01
+    if double_prob > prob_to_win:
         delta = - amount
         won = False
-    elif double_prob > prob_to_win:
+    elif double_prob < prob_to_win:
         delta = int(amount * 0.95)
         won = True
     else:
@@ -90,8 +90,8 @@ async def logic(interaction_user_id, interaction_guild_id, amount: int):
         if amount > 0.75 * balance:
             return DoubleResult(outcome=DoubleOutcome.TOO_HIGH)
 
-        double_luck = min(luck_factor * int(LOWEST_DOUBLE_PROB*0.5), int(HIGHEST_DOUBLE_PROB*0.5))
-        double_prob = random.randint(LOWEST_DOUBLE_PROB + int(double_luck), HIGHEST_DOUBLE_PROB)
+        double_luck = min(luck_factor * int(constants.LOWEST_DOUBLE_PROB*0.5), int(constants.HIGHEST_DOUBLE_PROB*0.5))
+        double_prob = random.randint(constants.LOWEST_DOUBLE_PROB + int(double_luck), constants.HIGHEST_DOUBLE_PROB)
         delta, won = _resolve_double_outcome(double_prob, amount)
         if delta is None:
             return DoubleResult(outcome=DoubleOutcome.DRAW)
